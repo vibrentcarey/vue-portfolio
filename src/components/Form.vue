57b987558cfadd063d679b290e79538169b4041e
Brent Carey <brentcarey@Brents-MacBook-Pro.local>
2020-10-06 01:03:57 -0400

    Added form component

---
+++ src/components/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='form'>
+    <input />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<style lang="css">
+  #form {
+    height: 60vh;
+    width: 50vw;
+    background-color: #FFAEAF;
+    border-radius: 1em;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
+  <div id="home">
     <h1>Home page</h1>
+    <button v-if="!showForm" @click='toggleForm'>Hire Me</button>
+    <Form v-else/>
   </div>
 </template>
 
 <script>
+import Form from "../components/Form";
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  components: { Form },
+  data() {
+    return {
+    showForm: false,
+    }
+  },
+  methods: {
+    toggleForm() {
+    this.showForm = !this.showForm;
+    }
+  }
+};
 </script>
 
 <style lang="css">
-  
+#home {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
 </style>
