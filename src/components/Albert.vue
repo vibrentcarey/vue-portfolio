5fd4df28acdece50618609beb83590aeff8e3dcb
Brent Carey <brentcarey@Brents-MacBook-Pro.local>
2020-10-19 19:19:21 -0400

    Added image

---
+++ src/components/Albert.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Albert</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Albert'
+}
+</script>
+
+<style lang="css">
+  
+</style>
---
+++ src/views/Hobbies.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="about">
+    <h1>My Hobbies Include</h1>
+    <div id="hobby-box">
+      <HobbyCard
+        v-for="hobby in hobbies"
+        :imgSrc="hobby.imageLink"
+        :key="hobby.title"
+        :title="hobby.title"
+        :description="hobby.description"
+        :route="hobby.route"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import HobbyCard from "@/components/HobbyCard";
+export default {
+  name: "Hobbies",
+  components: { HobbyCard },
+  data() {
+    return {
+      hobbies: [
+        {
+          title: "Piano",
+          imageLink: "piano.jpeg",
+          description: "I started learning this year!"
+        },
+        {
+          title: "Video Games",
+          imageLink: "game.jpeg",
+          description: "Mainly Super Smash Bros!"
+        },
+        {
+          title: "Ping Pong",
+          imageLink: "pong.jpeg",
+          description: "My favourite sport to play!"
+        },
+        {
+          title: "Cat Parenting",
+          imageLink: "albert-main.jpeg",
+          description: "My cat son Albert!",
+          route: "/albert"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="css">
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#hobby-box {
+  width: 80vw;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin: 2em;
+}
+</style>
